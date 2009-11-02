acee41d27a62dee29d333431df38bee16433ad7b
Matěj Koubík <mkoubik@gmail.com>
2009-11-02 15:34:15 +0100

    IOptionsRepository: all methods throw BackendException.

--- src/backend/IOptionsRepository.java
+++ src/backend/IOptionsRepository.java
@@ -5,6 +5,8 @@
 
 package backend;
 
+import exceptions.BackendException;
+
 /**
  * Repository for options/settings persistency.
  * @author matej
@@ -15,14 +17,16 @@ public interface IOptionsRepository {
      * Retrieving option
      * @param option option name
      * @return option value
+     * @throws BackendException
      */
-    public String getOption(String option);
+    public String getOption(String option) throws BackendException;
 
     /**
      * Saving option.
      * @param option  option name
      * @param value option value
+     * @throws BackendException
      */
-    public void setOption(String option, String value);
+    public void setOption(String option, String value) throws BackendException;
     
 }
