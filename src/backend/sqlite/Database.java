a0be7d9888da14eb09d95d56bad20ab72963feae
Matěj Koubík <mkoubik@gmail.com>
2009-11-02 14:53:26 +0100

    SqliteBackend: Wrappers od Connection methods in Database.

--- src/backend/sqlite/Database.java
+++ src/backend/sqlite/Database.java
@@ -6,6 +6,9 @@
 package backend.sqlite;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Class for communication with database - JDBC Connection wrapper.
@@ -17,6 +20,25 @@ public class Database {
 
     public Database(Connection c) {
         this.connection = c;
+    }
+
+    /**
+     * Wrapper.
+     * @return JDBC statement
+     * @throws SQLException
+     */
+    public Statement createStatement() throws SQLException {
+        return this.connection.createStatement();
+    }
+
+    /**
+     * Wrapper.
+     * @param sql SQL query.
+     * @return JDBC prepared statement
+     * @throws SQLException
+     */
+    public PreparedStatement prepareStatement(String sql) throws SQLException {
+        return this.connection.prepareStatement(sql);
     }
 
 }
