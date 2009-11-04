d8efbbb57864f37742f1847ebfe7a355e3aba94b
Matěj Koubík <mkoubik@gmail.com>
2009-11-04 22:49:32 +0100

    SqliteBackend: OptionsRepository saves and retrieves options.

--- src/backend/sqlite/repositories/OptionsRepository.java
+++ src/backend/sqlite/repositories/OptionsRepository.java
@@ -7,6 +7,10 @@ package backend.sqlite.repositories;
 
 import backend.IOptionsRepository;
 import backend.sqlite.db.Database;
+import backend.sqlite.db.EnhancedReslutSet;
+import exceptions.BackendException;
+import java.sql.SQLException;
+import java.util.Map;
 
 /**
  * Repository for options/settings persistency into sqlite database.
@@ -32,8 +36,19 @@ public class OptionsRepository implements IOptionsRepository {
      * @param option option name
      * @return option value
      */
-    public String getOption(String option) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public String getOption(String option) throws BackendException {
+        try {
+            option = Database.escapeString(option);
+            String sql = "SELECT value FROM options WHERE name = \""+option+"\"";
+            EnhancedReslutSet rs = db.executeQuery(sql);
+            Map<String, String> row = rs.fetch();
+            if (row!=null) {
+                return row.get("value");
+            }
+        } catch (SQLException ex) {
+            throw new BackendException(ex.getMessage());
+        }
+        return null;
     }
 
     /**
@@ -41,8 +56,18 @@ public class OptionsRepository implements IOptionsRepository {
      * @param option option name
      * @param value option value
      */
-    public void setOption(String option, String value) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void setOption(String option, String value) throws BackendException {
+        try {
+            option = Database.escapeString(option);
+            value = Database.escapeString(value);
+            String sql = "UPDATE options SET value = \"" + value + "\" WHERE name = \"" + option + "\";";
+            if (db.executeUpdate(sql) == 0) {
+                sql = "INSERT INTO options (name, value) VALUES (\"" + option + "\", \"" + value + "\");";
+                db.executeUpdate(sql);
+            }
+        } catch (SQLException ex) {
+            throw new BackendException(ex.getMessage());
+        }
     }
 
 }
